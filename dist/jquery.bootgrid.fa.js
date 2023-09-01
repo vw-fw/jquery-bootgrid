5567453e4975964aa20becc69e789788b1e22b74
softshield01 <devops@softshield.onmicrosoft.com>
2023-09-01 11:38:08 +0530

    m

---
+++ dist/jquery.bootgrid.fa.js
@@ -0,0 +1,19 @@
+/*! 
+ * jQuery Bootgrid v1.4.0 - 12/31/2021
+ * Copyright © 2014-2015 Rafael J. Staib; Copyright © 2018-2021 Deciso B.V. (http://www.jquery-bootgrid.com)
+ * Licensed under the MIT license. See LICENSE.txt for more details.
+ */
+;(function ($, window, undefined)
+{
+    /*jshint validthis: true */
+    "use strict";
+
+$.extend($.fn.bootgrid.Constructor.defaults.css, {
+    icon: "icon fa",
+    iconColumns: "fa-th-list",
+    iconDown: "fa-sort-desc",
+    iconRefresh: "fa-refresh",
+    iconSearch: "fa-search",
+    iconUp: "fa-sort-asc"
+});
+})(jQuery, window);
